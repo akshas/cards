3a2ce1219964e5c8492d5497500d13355547861c
Alex <allexxx81@gmail.com>
2019-01-05 01:15:27 +0100

    0.2 (editable element)

--- app/public/assets/main.js
+++ app/public/assets/main.js
@@ -51,73 +51,77 @@ $(function () {
   /**
    *  updeate collection name on the home page
    */
-  var form = $('.update-collection').each(function () {
-    console.log($(this).children('.new-name'));
 
-    $(this).on('submit', function (e) {
+  var renameButton = $('.rename').each(function () {
+    var editable = '.editable';
+    var $a = $(this).siblings('a'),
+      isEditable = $a.is(editable),
+      nameByFocus;
+    $(this).on('click', function () {
 
+      $a.prop('contenteditable', !isEditable).toggleClass('editable').focus();
+      nameByFocus = $a.html();
+      $('a').not($a).prop('contenteditable', false).removeClass('editable');
+      $('.rename').not(this).html('<img src="/assets/img/update-icon.png", alt="update collection">')
 
-      if ($(this).children('.new-name').is('.new-name')) {
-
-        $(this).children('.rename').html('<img src="/assets/img/update-icon.png" alt="update collection">');
-        var newName = $(this).children('.new-name').val();
-
+      if ($($a).is(editable)) {
+        $(this).html('<img src="/assets/img/confirm-icon.png", alt="confirm collection">');
+      } else {
         var data = {
-          id: $(this).attr("id"),
-          newName: newName
+          id: $(this).attr("data-rename-id"),
+          newName: $($a).text()
         };
         $.ajax({
           type: "PUT",
-          url: "/",
+          url: '/',
           data: data,
           success: function (data) {
-            location.reload();
+            location.reload(false);
           }
         });
         return false;
-      } else {
-        e.preventDefault();
-        /**
-         *  инпут вместо ссылки (инпут имеет высоту и ширину ссылки)
-         */
-        $(this).children('.rename').html('<img src="/assets/img/confirm-icon.png" alt="confirm changes">');
-        var value = $(this).siblings('a').html();
-        var aWidth = $(this).siblings('a').innerWidth();
-        var aHeight = $(this).siblings('a').innerHeight();
-        var input = $('<input type="text"  class="new-name" value="' + value + '" required selected oninvalid="this.setCustomValidity(\'You have to fill this field!\')" oninput="this.setCustomValidity(\'\')" >').insertBefore($(this).children('.rename')).select();
-        $(input).css({
-          'max-width': aWidth,
-          'max-height': aHeight
-        });
-
-        $(this).siblings('a').addClass('hide').hide();
-        $('.hide').not($(this).siblings('a')).show().removeClass('hide');
       }
 
-      $('.new-name').not($(this).children('.new-name')).siblings('.rename').html('<img src="/assets/img/update-icon.png" alt="update collection">');
-      $('.new-name').not($(this).children('.new-name')).remove();
-    });
+      $($a).on('blur', function () {
 
-  }); // end of the collection updating
-
-  /**
-   *  delete colltection on the home page
-   */
-  var deleteButton = $('.delete').each(function () {
-    $(this).on('click', function () {
-      var data = {
-        id: $(this).attr("data-delete-id")
-      };
-      console.log(data);
-      $.ajax({
-        type: "DELETE",
-        url: "/",
-        data: data,
-        success: function (data) {
-          location.reload(false);
+        if ($(this).html() != nameByFocus) {
+          var data = {
+            id: $(this).siblings('.rename').attr("data-rename-id"),
+            newName: $(this).text()
+          };
+          $.ajax({
+            type: "PUT",
+            url: '/',
+            data: data,
+            success: function (data) {
+              location.reload(false);
+            }
+          });
+          return false;
         }
       });
-      return false;
     });
   });
 }); // end of the collection delete
+
+/**
+ *  delete colltection on the home page
+ */
+var deleteButton = $('.delete').each(function () {
+  $(this).on('click', function () {
+    var data = {
+      id: $(this).attr("data-delete-id")
+    };
+    console.log(data);
+    $.ajax({
+      type: "DELETE",
+      url: "/",
+      data: data,
+      success: function (data) {
+        location.reload(false);
+      }
+    });
+    return false;
+  });
+});
+// end of the collection delete
